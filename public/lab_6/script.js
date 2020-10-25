c3035c198dfd24784dd6a6fb592fd53dc75101b5
Dimeji98 <98olayinkadimeji@gmail.com>
2020-10-24 23:45:30 -0400

    lab 6

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,5 +1,11 @@
 // You may wish to find an effective randomizer function on MDN.
 
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+}
+
 function range(int) {
   const arr = [];
   for (let i = 0; i < int; i += 1) {
@@ -30,6 +36,25 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
+      const array= range(10);
+      const array2= array.map(()
+      => { const number = rand(0,243);
+        return fromServer[number];
+      });
+      const list = array2.sort((a,b)=>sortFunction(b,a, 'name'));
+      const ol = document.createElement('ol');
+      ol.className = 'flex-inner';
+      
+      for (var i = 0; i < list.length; i++) {
+       
+        var listitem = document.createElement('li');
+        
+      
+        listitem.appendChild(document.createTextNode(list[i]));
+
+        
+        ol.appendChild(listitem);
+    }
       console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.route('/api')
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    res.status(200).send('Hello World');
+    res.json(countries);
   });
 
 app.listen(port, () => {
